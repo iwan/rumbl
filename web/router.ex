3f2c3d133201552049937c053232e81e6fb483d4
Iwan Buetti <iwan.buetti@gmail.com>
2017-11-23 09:26:14 +0100

    chapter 4 completed

--- web/router.ex
+++ web/router.ex
@@ -15,8 +15,10 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    # get "/users", UserController, :index
+    # get "/users/new", UserController, :new
+    # get "/users/:id", UserController, :show
     get "/", PageController, :index
   end
 
